d44a6a8372920b3e558f290ef2b02bbf8d3ea373
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-03-22 17:24:31 +0000

    all

--- src/components/gameSearch.js
+++ src/components/gameSearch.js
@@ -19,30 +19,30 @@ class GameSearch extends React.Component {
   }
 
   //ADD DATA FROM GAMES WE'RE PULLING HERE TO USE IN SELECT BOX
+
   gameSearch() {
     axios.post('/api/games', this.state.query)
-      .then(result => {
-        console.log(result)
-
-      })
-      .then(res => console.log(res))
+      .then(res => res.data.map(game => ({ value: game.id, label: game.name})))
+      .then(games => this.setState({ games }))
       .catch(err => console.log(err))
   }
 
-  handleChange() {
-    console.log('hello')
-    this.setState({ query: this.search.value })
+
+  handleChange({ target: { value } }) {
+    this.setState({...this.state, query: value }, () => console.log(this.state))
+    this.gameSearch()
   }
 
   render() {
+    const { games } = this.state
     return(
       <div className="main-gameSearch">
         <div className="contains-gameSearch">
           <h2> Search a game and get involved! </h2>
-          <Select
+          <input
             className="gameSearchBar"
-            games={this.state.games}
             onChange={this.handleChange}
+            options={games}
           />
           <button className="gameSearchButton"> Go discover </button>
         </div>
