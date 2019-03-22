e5ee587b8f231230710d29541215c755451abe6f
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-03-22 11:38:04 +0000

    added ign news route

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const router = require('express').Router()
 const auth = require('../controllers/auth')
+const rp = require('request-promise')
 
 router.post('/register', auth.register)
 router.post('/login', auth.login)
+
+const ignApiKey = process.env.IGN_NEWS_KEY
+
+function getNews(req, res) {
+  rp({
+    url: `https://newsapi.org/v2/top-headlines?sources=ign&apiKey=${ignApiKey}`,
+    method: 'GET',
+    qs: req.query,
+    json: true
+  })
+    .then(news => res.json(news))
+    .catch(err => res.json(err))
+}
+
+router.get('/news', getNews)
 
 module.exports = router
