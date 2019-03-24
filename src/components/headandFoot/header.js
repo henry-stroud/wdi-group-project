dabc73c5be9d51460cc11651dccfd18cce483d7e
mdnrosen <mdnrosen@hotmail.co.uk>
2019-03-24 16:42:40 +0000

    swapped back nav bar

--- src/components/headandFoot/header.js
+++ src/components/headandFoot/header.js
@@ -12,14 +12,11 @@ class Header extends React.Component {
     return (
       <div>
         <header>
-          <div className="contains-loggedin">
-            <img className="avatar" src="https://gaming-logo-maker.com/wp-content/uploads/2018/11/avatar-maker-minecraft-character-02-300x300.gif" alt=""/>
-            <div className="userloggedin">
-              <Link to='/viewprofile'> <h4> bubblesaurus90 </h4> </Link>
-              <h5> currently playing... </h5>
-            </div>
-          </div>
-
+          <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Log In</Link>
+          </nav>
           <div className="contains-titleLogo">
             <h1>Player Connect</h1>
             <img src="https://i.pinimg.com/originals/aa/8d/46/aa8d4679b3b39e91b17cb4675ec0d60d.jpg" alt="playerconnect logo"/>
