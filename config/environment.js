8e911d36e6a7138286669cdaea159da1057c0515
Buse Uslu <buseuslu24@gmail.com>
2019-03-29 13:42:58 +0000

    deployment changes

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'dev'
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGO_URI || `mongodb://localhost/playerconnect-${env}`
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/playerconnect-${env}`
 const secret = process.env.SECRET || 'bhm'
 
 module.exports = { env, dbURI, port, secret }
